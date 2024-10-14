2f70a1bc21a260f9cd32a1c6256749a047cff528
priyankasharma1603 <psgeeta16@gmail.com>
2024-10-14 13:01:00 +0530

    Merged remote changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import Dashboard from './pages/Dashboard';
 
+
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<SignupPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <React.StrictMode>
+        <Routes>
+          <Route path="/" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          
+        </Routes>
+      </React.StrictMode>
     </Router>
   );
 };
